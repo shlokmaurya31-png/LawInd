6e1b0e5e6625b7b3e815d417e30ff11d613a0091
aaryan <shlokmaourya@shloks-MacBook-Pro-2378.local>
2026-05-28 02:23:15 +0100

    add justice hero image

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='.')
 def home():
     return send_from_directory('.', 'index.html')
 
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory('.', filename)
+
 if __name__ == '__main__':
     app.run(debug=True)
