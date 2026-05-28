a30fdd29db9ed50284254dadf688eb3a86dd268c
aaryan <shlokmaourya@shloks-MacBook-Pro-2378.local>
2026-05-28 06:09:28 +0100

    add ROI calculators, blog, resources pages

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__, static_folder='.')
 def home():
     return send_from_directory('.', 'index.html')
 
+@app.route('/roi/law-firm')
+def roi_firm():
+    return send_from_directory('.', 'roi-firm.html')
+
+@app.route('/roi/in-house')
+def roi_inhouse():
+    return send_from_directory('.', 'roi-inhouse.html')
+
+@app.route('/blog')
+def blog():
+    return send_from_directory('.', 'blog.html')
+
+@app.route('/resources')
+def resources():
+    return send_from_directory('.', 'resources.html')
+
 @app.route('/<path:filename>')
 def static_files(filename):
     return send_from_directory('.', filename)
